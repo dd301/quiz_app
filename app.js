803bde0b7235679d2b3487b6f9a14bc64d1ab1ef
dd301 <donatodoley1@gmail.com>
2022-10-11 21:46:28 +0530

    added scoring, feedback and end message

--- app.js
+++ app.js
@@ -1,34 +1,66 @@
 var getQuiz = function () {
-    var qIndex = 0;
-    var questions = [
-        {
-            question: "What is the highest mountain in the world?",
-            answer: "Everest"
-        },
-        {
-            question: "What is the highest mountain in Scotland?",
-            answer: "Ben Nevis"
-        },
-        {
-            question: "How many munros are in Scotland?",
-            answer: "284"
-        }
+    var score = 0,
+        qIndex = 0,
+        inPlay = true,
+        questions,
+        next,
+        getQuestion,
+        checkAnswer,
+        submit;
+      
+    questions = [
+      {
+        question: "What is the highest mountain in the world?",
+        answer: "Everest"
+      },
+      {
+        question: "What is the highest mountain in Scotland?",
+        answer: "Ben Nevis"
+      }
     ];
-
+    
+    next = function () {
+      qIndex = qIndex + 1;
+      
+      if (qIndex >= questions.length) {
+        inPlay = false;
+        console.log("You have finished the quiz.");
+      }
+    };
+    
+    getQuestion = function () {
+      if (inPlay) {
+        return questions[qIndex].question;
+      } else {
+        return "You have finished the quiz.";
+      }
+    };
+    
+    checkAnswer = function (userAnswer) {
+      if (userAnswer === questions[qIndex].answer) {
+        console.log("Correct!");
+        score = score + 1;
+      } else {
+        console.log("No, the answer is " + questions[qIndex].answer);
+      }
+    };
+    
+    submit = function (userAnswer) {
+      var message = "You have finished the quiz.";
+      
+      if (inPlay) {
+        checkAnswer(userAnswer);
+        next();
+        message = "Your score is " + score + " out of " + qIndex;
+      }
+        
+      return message;
+    };
+    
     return {
-        quizMe : function () {
-            return questions[qIndex].question;
-        },
-
-        showMe : function () {
-            return questions[qIndex].answer;
-        },
-
-        next : function () {
-            qIndex = qIndex + 1;
-            return "Ok";
-        }
+      quizMe: getQuestion,
+      submit: submit
     };
 };
-
+  
 var quiz = getQuiz();
