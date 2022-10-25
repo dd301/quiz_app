d9b778e4a3f02d43ca9880996805241469731fcc
dd301 <donatodoley1@gmail.com>
2022-10-25 17:51:58 +0530

    added new feature
    allows asking of random questions

---
+++ features/random/app.js
@@ -0,0 +1,31 @@
+var getQuiz = function () {
+    var qIndex = 0;
+    
+    var questions = [
+      { question: "7 x 8", answer: "56" },
+      { question: "12 x 12", answer: "144" },
+      { question: "5 x 6", answer: "30" },
+      { question: "9 x 3", answer: "27" }
+    ];
+    
+    var getQuestion = function () {
+      qIndex = between(0, questions.length - 1);
+      return questions[qIndex].question;
+    };
+    
+    var checkAnswer = function (userAnswer) {
+      if (userAnswer === questions[qIndex].answer) {
+        return "Correct!";
+      } else {
+        return "No, the answer is " + questions[qIndex].answer;
+      }
+  };
+    
+    return {
+      quizMe: getQuestion,
+      submit: checkAnswer
+    };
+  };
+  
+  var quiz = getQuiz();
+  
